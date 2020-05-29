7f68fd6ad70e2cbb07d8ab75efabb97c04216079
slimane rechdi <slimane.rechdi@gmail.com>
2020-05-29 15:26:38 -0400

    create a folder

--- pagination_vm/__main__.py
+++ pagination_vm/__main__.py
@@ -1,34 +1,69 @@
-
 #! python3
 
+import os
 import click
-from PyPDF4 import PdfFileReader, PdfFileWriter
-from PyPDF4.pdf import PageObject
-
-import reportlab
-from reportlab.lib.units import inch
+from PyPDF4 import PdfFileReader
 from reportlab.pdfgen import canvas
-#import pikepdf (explorings)
+from reportlab.lib.units import inch
+
+from pagination_vm.merge import paginate_pdf
+
+# Need to create a blank page a the beginning and remove -1 to the pagination.
+
+def create_template_pagination(name_file, total_pages):
+    c = canvas.Canvas(name_file)
+    c.setPageSize(
+        (17*inch, 11*inch) # Tabloid format
+    )
+    for _ in range(total_pages):
+        page_num = c.getPageNumber()
+        text = "%s/%s" % (page_num, total_pages)
+        c.drawString(
+            16.4*inch,
+            0.15*inch,
+            text,
+        )
+        c.showPage()
+    c.save()
 
 
 @click.command(help="Add pagination 1/n at bottom-right of a pdf file.")
 @click.argument("pdf_name", nargs=1)
 def numeration(pdf_name):
-    print(f"PDF Name: {pdf_name}")
+
     pdf = PdfFileReader(pdf_name)
     number_total_of_pages = pdf.getNumPages()
-    print(f"Number total of pages: {number_total_of_pages}")
 
-    c = canvas.Canvas("pagination.pdf")
-    c.setPageSize((17*inch, 11*inch)) #paper format: Tabloid
-    for _ in range(number_total_of_pages):
-        page_num = c.getPageNumber()
-        text = "%s/%s" % (page_num, number_total_of_pages)
-        c.drawString( 16.4*inch, 0.15*inch, text)
-        c.showPage()
-    c.save()
+    print(f"PDF Name .............: {pdf_name}")
+    print(f"Total number of pages : {number_total_of_pages}")
+
+    # Create a folder paginatedPDFs where the new pdfs will be loaded.
+    os.makedirs(r"./paginatedPDFs", exist_ok=True)
+
+    # Create the layer with footer.
+    create_template_pagination(
+        "pagination.pdf",
+        number_total_of_pages
+    )
+
+    # Create the new pdf merging two layers:  footer + manual
+    paginate_pdf(
+        pdf_name,
+        number_total_of_pages,
+        "pagination.pdf",
+    )
+
+    # Delete the pdf template
+    os.remove("pagination.pdf")
 
 
 if __name__ == "__main__":
     numeration()
 
+
+#TODO: implement library prompt_toolkit, click option
+#TODO: manage 1 or 2 pages index
+#TODO: click - on file , many files, or all files in folders.
+#TODO: display with info and instructions at beginning
+#TODO: handling missed used
+#TODO: review of varialbles names
--- pagination_vm/merge.py
+++ pagination_vm/merge.py
@@ -2,18 +2,47 @@ from PyPDF4 import PdfFileReader, PdfFileWriter
 from PyPDF4.pdf import PageObject
 
 
-reader = PdfFileReader(open("1.pdf",'rb'))
-invoice_page = reader.getPage(0)
-sup_reader = PdfFileReader(open("2.pdf",'rb'))
-sup_page = sup_reader.getPage(0)  # We pick the second page here
+def paginate_pdf(pdf_name, number_page, pagination_template):
+    """Create a new pdf with the pagination footer
+	starting from the second page of the manual.
+	"""
 
-translated_page = PageObject.createBlankPage(None, sup_page.mediaBox.getWidth(), sup_page.mediaBox.getHeight())
-translated_page.mergeScaledTranslatedPage(sup_page, 1, 0, -400)  # -400 is approximate mid-page
+    writer = PdfFileWriter()
 
-translated_page.mergePage(invoice_page)
+    stream_manuals = open(pdf_name, "rb")
+    manuals = PdfFileReader(stream_manuals)
+    stream_pagination = open(pagination_template, "rb")
+    pagination = PdfFileReader(stream_pagination)
 
-writer = PdfFileWriter()
-writer.addPage(translated_page)
+    # TODO: adjuste the start and end of the pagination
 
-with open('out.pdf', 'wb') as f:
-	writer.write(f)
+    for i in range(number_page):
+        manuals_page = manuals.getPage(i)  # pageNumber: 0
+        pagination_page = pagination.getPage(i)
+
+        # Stack blank page
+        translated_page = PageObject.createBlankPage(
+            None,
+            width=manuals_page.mediaBox.getHeight(),  # width = 1224
+            height=manuals_page.mediaBox.getWidth(),  # height = 792
+        )
+
+        # Stack pagination
+        translated_page.mergePage(pagination_page)
+
+        # Stack manual
+        translated_page.mergeRotatedScaledTranslatedPage(
+            manuals_page, rotation=-90, scale=1, tx=0, ty=792, expand=True
+        )
+
+        writer.addPage(translated_page)
+
+    pdf_out = "./paginatedPDFs/"+pdf_name  # New name of the output pdf file.
+
+    with open(pdf_out, "wb") as _:
+        writer.write(_)
+
+    stream_manuals.close()
+    stream_pagination.close()
+
+    print(f"[CREATED] {pdf_out}")
